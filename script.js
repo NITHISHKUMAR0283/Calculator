a77f3478863d7087788b957f6ae6a4ee34f49d74
NITHISHKUMAR0283 <nk0283@srmist.edu.in>
2025-11-08 16:56:21 +0530

    added loic( add, mult,ect) when the button is clicked

--- script.js
+++ script.js
@@ -13,6 +13,66 @@ function Append_Buttons(){
         button.textContent=i;
         Operator_container.appendChild(button);
     }
+    Button_container.addEventListener("click",function(event){
+        numbers(event);
+    });
+    Operator_container.addEventListener("click",function(event1){
+        numbers(event1);
+    });
 
 }
 Append_Buttons();
+let expression="";
+let operator_count=0;
+let last_operator="";
+function numbers(event){
+    clickedButton=event.target;
+    text=clickedButton.textContent.trim();
+
+    if (/^[+\-x/%=]$/.test(text)){
+        if(operator_count==1 ){
+            eval_expression();
+
+              }
+        if(operator_count==0){
+            if(text!="="){
+            expression+=text;
+            operator_count+=1; }       }   
+        }   
+    if(/^[1-9]$/.test(text)){
+
+        expression+=text;
+    } 
+        console.log(expression);
+    
+
+    
+}
+function operators(event){
+    clickedoperator=event.target;
+}
+function eval_expression(){
+    let result=0;
+    let op=""
+    let str=""
+    for (let ele of expression){
+        if(/^[+\-x/%]$/.test(ele)){
+            result=Number(str);
+
+            op+=ele;
+            str="";
+        }       
+        else{
+            str+=ele;
+        }
+    }
+    let operand2=Number(str);
+    if(op=="+")result+=operand2;
+    else if(op=="-")result-=operand2;
+    else if(op=="x")result*=operand2;
+    else if(op=="/")result/=operand2;
+    else if(op=="%")result%=operand2;
+    operator_count=0;
+    expression=result;
+
+}
