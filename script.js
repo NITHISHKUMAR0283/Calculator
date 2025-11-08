e8c9c4051426f35d2434fd1ade5bd05264b9e47e
NITHISHKUMAR0283 <nk0283@srmist.edu.in>
2025-11-08 12:23:46 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function Append_Buttons(){
+    let buttons_text="DEL C % 7 8 9 4 5 6 1 2 3 0 . =".split(" ");
+    let operator_text="/ x - +".split(" ");
+    let Button_container=document.getElementById("numbers");
+    let Operator_container=document.getElementById("operators");
+    for(let i of buttons_text){
+        let  button=document.createElement("Button");
+        button.textContent=i;
+        Button_container.appendChild(button);
+    }
+    for(let i of operator_text){
+        let button=document.createElement("Button");
+        button.textContent=i;
+        Operator_container.appendChild(button);
+    }
+
+}
+Append_Buttons();
+console.log(document.documentElement.outerHTML)
